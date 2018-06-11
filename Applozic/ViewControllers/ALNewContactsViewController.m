6ada15adb9b52b309e908bad749e3a433795fb43
malavika <malavika.akhilesh@titechnologies.org>
2018-06-11 12:14:34 +0530

    recommit the change

--- Applozic/ViewControllers/ALNewContactsViewController.m
+++ Applozic/ViewControllers/ALNewContactsViewController.m
@@ -405,19 +405,6 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-//    NSUInteger count = self.filteredContactList.count;
-//    if(self.selectedSegment == 1)
-//    {
-//        count = self.filteredContactList.count;
-//    }
-//    if(count == 0)
-//    {
-//        if(![self.activityIndicator isAnimating]){
-//            [self.emptyConversationText setHidden:NO];
-//            [self setTextForEmpty];
-//        }
-//    }
-//    return count;
     return 5;
 }
 
